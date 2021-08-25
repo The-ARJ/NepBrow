26ea216690f672f25428837706373384fed27967
Aayush <joshiaayush871@gmail.com>
2021-08-25 20:33:35 +0545

    Initial commit

---
+++ src/main/java/browser/nepbrowagain/HelloApplication.java
@@ -0,0 +1,37 @@
+package browser.nepbrowagain;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ArifMahmud
+ */
+public class HelloApplication extends Application {
+
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("Light Browser");
+        stage.getIcons().add(new Image("file:Resources/icon256.png"));
+        stage.show();
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module browser.nepbrowagain {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires javafx.graphics;
+    requires java.sql;
+
+    opens browser.nepbrowagain to javafx.fxml;
+    exports browser.nepbrowagain;
+}
