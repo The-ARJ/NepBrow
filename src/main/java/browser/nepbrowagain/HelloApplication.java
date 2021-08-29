e08b39dbc5cec41b935fbcddb811d828522737b4
Nirajan <gnirajan46@gmail.com>
2021-08-29 14:21:15 +0545

    commit

--- src/main/java/browser/nepbrowagain/HelloApplication.java
+++ src/main/java/browser/nepbrowagain/HelloApplication.java
@@ -12,7 +12,7 @@ import javafx.stage.Stage;
 public class HelloApplication extends Application {
 
 
-    /*creating new scene*/
+    /*Creating new scene*/
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
