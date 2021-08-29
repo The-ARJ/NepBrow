c2f5420b31cef634664609493505f3b52e63a435
Aayush <joshiaayush871@gmail.com>
2021-08-29 14:25:36 +0545

    application title

--- src/main/java/browser/nepbrowagain/HelloApplication.java
+++ src/main/java/browser/nepbrowagain/HelloApplication.java
@@ -20,7 +20,7 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
-        stage.setTitle("NepBrow");
+        stage.setTitle("NepBrow");//title
         stage.getIcons().add(new Image("file:Resources/browser.png"));
         stage.show();
     }
