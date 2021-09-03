b6a001eba2bcff126aedca8374a026e4ba9be3db
ASUS <shahdharmendrakumar209@gmail.com>
2021-09-03 15:11:08 +0545

    improvement

--- src/main/java/browser/nepbrowagain/HelloApplication.java
+++ src/main/java/browser/nepbrowagain/HelloApplication.java
@@ -20,7 +20,7 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
-        stage.setTitle("NepBrow");//title of browser
+        stage.setTitle("NepBrow");//title of web  browser
         stage.getIcons().add(new Image("file:Resources/browser.png"));
         stage.show();
     }
