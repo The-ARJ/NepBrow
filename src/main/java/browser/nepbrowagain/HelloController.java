58493adaa9e900b41d1511700b907a8cf22bc48e
Aayush <joshiaayush871@gmail.com>
2021-09-03 13:05:05 +0545

    improvement

--- src/main/java/browser/nepbrowagain/HelloController.java
+++ src/main/java/browser/nepbrowagain/HelloController.java
@@ -20,16 +20,10 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.EventListener;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.ResourceBundle;
-import java.util.SortedSet;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
+
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -49,19 +43,16 @@ import javafx.concurrent.Worker.State;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Side;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.ContextMenu;
-import javafx.scene.control.CustomMenuItem;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.ToolBar;
@@ -84,10 +75,6 @@ public class HelloController implements Initializable {
 
     @FXML
     TabPane tabPane;
-
-
-    @FXML
-    MenuItem homePageBackgroundImg;
 
     @FXML
     Label downloadStatusLabel;
@@ -326,13 +313,13 @@ public class HelloController implements Initializable {
             downloadStatusLabel.setText("File download complete");
         }
     }
-    //create content for downlaods
+    //create content for downloads
     private Parent createContent() {
         VBox root = new VBox();
         root.setPrefSize(300, 400);
 
         TextField fieldURL = new TextField();
-        fieldURL.setPromptText("⚭ enter download link here");
+        fieldURL.setPromptText("Enter Download Link here");
         root.getChildren().addAll(fieldURL);
 
         fieldURL.setOnAction(event -> {
@@ -355,77 +342,8 @@ public class HelloController implements Initializable {
 
     public class AutoCompleteTextField extends TextField{
         /** The existing auto complete entries. */
-        private final SortedSet<String> entries;
-        /** The popup used to select an entry. */
-        private ContextMenu entriesPopup;
-
-        /** Construct a new AutoCompleteTextField. */
-        public AutoCompleteTextField() {
-            super();
-            entries = new TreeSet<>();
-            entriesPopup = new ContextMenu();
-            textProperty().addListener(new ChangeListener<String>(){
-                @Override
-                public void changed(ObservableValue<? extends String> observableValue, String s, String s2) {
-                    if (getText().length() == 0){
-                        entriesPopup.hide();
-                    } else{
-                        LinkedList<String> searchResult = new LinkedList<>();
-                        final List<String> filteredEntries = entries.stream().filter(e -> e.toLowerCase().contains(getText().toLowerCase())).collect(Collectors.toList());
-                        entries.add("apple.com"); //entries.add("a"); entries.add("aa"); entries.add("aaa"); entries.add("aab"); entries.add("aac"); entries.add("BBC");
-                        entries.add("bing.com"); entries.add("google.com"); entries.add("microsoft.com"); entries.add("yahoo.com"); entries.add("facebook.com");
-                        searchResult.addAll(entries);
-                        if (entries.size() > 0){
-                            populatePopup(searchResult);
-                            if (!entriesPopup.isShowing()){
-                                entriesPopup.show(AutoCompleteTextField.this, Side.BOTTOM, 0, 0);
-                            }
-                        } else {
-                            entriesPopup.hide();
-                        }
-                    }
-                }
-            });
-
-            focusedProperty().addListener(new ChangeListener<Boolean>() {
-                @Override
-                public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean aBoolean2) {
-                    entriesPopup.hide();
-                }
-            });
-
-        }
 
 
-        // Get the existing set of auto complete entries.
-
-        public SortedSet<String> getEntries() { return entries; }
-
-        // Populate the entry set with the given search results.  Display is limited to 10 entries, for performance.
-
-        private void populatePopup(List<String> searchResult) {
-            List<CustomMenuItem> menuItems = new LinkedList<>();
-            // If  like more entries,   this line.
-            int maxEntries = 10;
-            int count = Math.min(searchResult.size(), maxEntries);
-            for (int i = 0; i < count; i++)
-            {
-                final String result = searchResult.get(i);
-                Label entryLabel = new Label(result);
-                CustomMenuItem item = new CustomMenuItem(entryLabel, true);
-                item.setOnAction(new EventHandler<ActionEvent>()
-                {
-                    @Override
-                    public void handle(ActionEvent actionEvent) {
-                        setText(result);
-                        entriesPopup.hide();
-                    }
-                });
-                menuItems.add(item);
-            }
-            entriesPopup.getItems().clear();
-            entriesPopup.getItems().addAll(menuItems);
-        }
     }
 
     class SearchEngine{
@@ -578,7 +496,7 @@ public class HelloController implements Initializable {
 
             urlBox.setPromptText("🔎 Enter URL Here");
             urlBox.setPrefHeight(30);
-            urlBox.setPrefWidth(391);
+            urlBox.setPrefWidth(700);
             goButton.setPrefHeight(30);
             goButton.setPrefWidth(60);
             goButton.setStyle("-fx-background-color: #66FFCC; ");
