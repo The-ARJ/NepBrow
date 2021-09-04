747d6457ebd61c6a437a0ba77b657a2cf341c73e
Aayush <joshiaayush871@gmail.com>
2021-09-04 14:42:25 +0545

    improvement

--- src/main/java/browser/nepbrowagain/LoginController.java
+++ src/main/java/browser/nepbrowagain/LoginController.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;//login
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class LoginController {
