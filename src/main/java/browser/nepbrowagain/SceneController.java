ae1cf606928b6f3f6cd48e8d37f35e2d88265e10
Aayush <joshiaayush871@gmail.com>
2021-09-05 17:55:14 +0545

    Login and registration jdbc connection successful

--- src/main/java/browser/nepbrowagain/SceneController.java
+++ src/main/java/browser/nepbrowagain/SceneController.java
@@ -1,5 +1,6 @@
 package browser.nepbrowagain;
 
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXMLLoader;
@@ -12,20 +13,30 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+import javax.swing.plaf.nimbus.State;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class SceneController {
     @FXML
     JFXButton SignIN,SignUP,AlreadyHaveAccount,CreateNewAccount;
 
-    @FXML
+    @FXML//userLogin
     TextField UserName;
 
-    @FXML
-    PasswordField Password;
+    @FXML //registration
+    TextField Full_Name,user_name,email;
 
     @FXML
-    Label LoginMessage;
+    PasswordField Password,password;
+
+    @FXML
+    Label LoginMessage;//signin
+
+    @FXML//signup
+    Label signuplabel,signuplabel2;
 
     private Stage stage;
     private Scene scene;
@@ -35,11 +46,32 @@ public class SceneController {
 
     //SIGNUP/REGISTRATION BUTTON FUNCTION
     public void handlebutton1() throws IOException {
+        String full_name = Full_Name.getText();
+        String user_Name = user_name.getText();
+        String user_email = email.getText();
+        String user_password = password.getText();
+
+
+
+        //USER REGISTRATION AND SIGN UP BUTTON FUNCTION
         SignUP.setOnAction(e ->{
-            Alert pop = new Alert(Alert.AlertType.INFORMATION);
-            pop.setTitle("Done");
-            pop.setHeaderText("SignUp Successful,Click OK");
-            pop.show();
+            System.out.println("Button pressed");
+            JDBC jdbc = new JDBC();
+
+
+            if(Full_Name.getText().isBlank()==true && user_name.getText().isBlank()==true && email.getText().isBlank()==true && password.getText().isBlank()==true) {
+                signuplabel.setText("Please Enter Credentials");}
+
+            if(Full_Name.getText().isBlank()==false && user_name.getText().isBlank()==false && email.getText().isBlank()==false && password.getText().isBlank()==false) {
+                String sql = "Insert Into tbl_registration(FullName,UserName,Email,Password) Values('"+full_name+"','"+user_Name+"','"+user_email+"','"+user_password+"')";
+                int ans = jdbc.insert(sql);
+                if (ans>0) {
+                    signuplabel.setVisible(false);
+                    signuplabel2.setText("Sign Up Completed");
+
+
+                }
+            }
         });
     }
 
@@ -62,7 +94,32 @@ public class SceneController {
 
     //Sign In Function in User Login Window
     public void handlebutton4() throws IOException {
-        Stage Window = (Stage) SignIN.getScene().getWindow();
+        JDBC jdbc = new JDBC();
+        String userr_Name = UserName.getText();
+        String userr_password = Password.getText();
+
+        String verifyLogin ="Select count(1) FROM tbl_registration WHERE UserName = '"+userr_Name+"' AND Password = '"+userr_password+"'";
+
+        try{
+            ResultSet queryResult = jdbc.select(verifyLogin);
+            while((queryResult.next())){
+                if (queryResult.getInt(1)==1){
+                    LoginMessage.setText("Login Successful");
+                    Stage Window = (Stage) SignIN.getScene().getWindow();
+                    Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+                    Window.setScene(new Scene(root));
+                }
+                else{
+                    LoginMessage.setText("User Name or Password do not match");
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
         if (UserName.getText().isBlank()==true && Password.getText().isBlank()==true){
             LoginMessage.setText("Please Enter User Name And Password");
         }
@@ -73,8 +130,7 @@ public class SceneController {
             LoginMessage.setText(("Please Enter Your Password"));
         }
 
-//        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
-//        Window.setScene(new Scene(root));
+
 
     }
 
