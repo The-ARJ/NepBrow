7b3c885a1aa2b45a799ca2b0e8689475ac996859
Aayush <joshiaayush871@gmail.com>
2021-09-05 14:48:28 +0545

    jdbcconnector

--- src/main/java/browser/nepbrowagain/SceneController.java
+++ src/main/java/browser/nepbrowagain/SceneController.java
@@ -7,6 +7,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,11 +18,22 @@ public class SceneController {
     @FXML
     JFXButton SignIN,SignUP,AlreadyHaveAccount,CreateNewAccount;
 
+    @FXML
+    TextField UserName;
+
+    @FXML
+    PasswordField Password;
+
+    @FXML
+    Label LoginMessage;
+
     private Stage stage;
     private Scene scene;
     private Parent root;
 
 
+
+    //SIGNUP/REGISTRATION BUTTON FUNCTION
     public void handlebutton1() throws IOException {
         SignUP.setOnAction(e ->{
             Alert pop = new Alert(Alert.AlertType.INFORMATION);
@@ -29,6 +43,7 @@ public class SceneController {
         });
     }
 
+    //Create New Account Function
     public void handlebutton2() throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("registration.fxml"));
         Stage Window = (Stage) CreateNewAccount.getScene().getWindow();
@@ -37,6 +52,7 @@ public class SceneController {
     }
 
 
+    //Already have account Function
     public void handlebutton3() throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         Stage Window = (Stage) AlreadyHaveAccount.getScene().getWindow();
@@ -44,10 +60,21 @@ public class SceneController {
 
     }
 
+    //Sign In Function in User Login Window
     public void handlebutton4() throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
         Stage Window = (Stage) SignIN.getScene().getWindow();
-        Window.setScene(new Scene(root));
+        if (UserName.getText().isBlank()==true && Password.getText().isBlank()==true){
+            LoginMessage.setText("Please Enter User Name And Password");
+        }
+        else if (UserName.getText().isBlank()==true){
+            LoginMessage.setText(("Please Enter Your User Name"));
+        }
+        else if (Password.getText().isBlank()==true){
+            LoginMessage.setText(("Please Enter Your Password"));
+        }
+
+//        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+//        Window.setScene(new Scene(root));
 
     }
 
